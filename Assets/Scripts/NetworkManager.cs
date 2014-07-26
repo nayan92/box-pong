9331abb040adb2f35bcdf39b381d66b55b28b532
Nayan Chudasama <nayan.chudasama11@gmail.com>
2014-07-26 02:09:43 +0100

    Added paddle and networking code for a single player.

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+	private const string typeName = "Box_Pong";
+	private const string gameName = "Room";
+	
+	private HostData[] hostList;
+	
+	public GameObject playerPrefab;
+	
+	void OnGUI() {
+		if (!Network.isClient && !Network.isServer) {
+			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server")) {
+				StartServer();
+			}
+			
+			if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts")) {
+				RefreshHostList();
+			}
+			
+			if (hostList != null) {
+				for (int i = 0; i < hostList.Length; i++) {
+					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName)) {
+						JoinServer(hostList[i]);
+					}
+				}
+			}
+		}
+	}
+	
+	private void StartServer() {
+		Network.InitializeServer(5, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+	
+	void OnServerInitialized() {
+		SpawnPlayer();
+	}
+	
+	private void RefreshHostList() {
+		MasterServer.RequestHostList(typeName);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent)	{
+		if (msEvent == MasterServerEvent.HostListReceived) {
+			hostList = MasterServer.PollHostList();
+		}
+	}
+	
+	private void JoinServer(HostData hostData) {
+		Network.Connect(hostData);
+	}
+	
+	void OnConnectedToServer() {
+		SpawnPlayer();
+	}
+	
+	private void SpawnPlayer() {
+		Network.Instantiate(playerPrefab, Vector3.up, Quaternion.identity, 0);
+	}
+}
